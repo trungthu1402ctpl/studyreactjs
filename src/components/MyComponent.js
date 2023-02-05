ae3d4b199ad8c1f97baf0f6c673f9dff4360ed17
Heimdall <trungthu1402ctpl@gmail.com>
2023-02-05 16:57:05 +0700

    demo with state in reactjs

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,8 +1,17 @@
 import React from "react";
 
-const MyComponent = () => {
-  return <h1>This is my Component 1.</h1>;
-};
-
-MyComponent();
+class MyComponent extends React.Component {
+  state = {
+    name: "ThuNT39",
+    age: 27,
+  };
+  render() {
+    return (
+      <div>
+        Hello mọi người, chào mừng mọi người đến với blog của mình Mình tên là
+        {this.state.name} và mình {this.state.age} tuổi.
+      </div>
+    );
+  }
+}
 export default MyComponent;
